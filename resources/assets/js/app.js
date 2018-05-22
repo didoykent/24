dd52f055a8863b907023b090e9ed440597d6fed1
didoykent <XeniaAim@yahoo.com>
2018-05-22 13:29:15 +0900

    fourth commit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -19,7 +19,7 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify);
-Vue.use(VueSocketio, 'http://localhost:8890');
+Vue.use(VueSocketio, '188.166.241.172:8890');
 
 axios.interceptors.request.use(function(config){
 
